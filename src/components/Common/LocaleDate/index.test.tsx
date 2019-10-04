92ae363bfe0a2e52edf1fab56e1a22cae109f551
fernandomg <fernando.greco@gmail.com>
2019-10-04 10:53:17 -0300

    Update tests to use `jest-dom`

--- src/components/Common/LocaleDate/index.test.tsx
+++ src/components/Common/LocaleDate/index.test.tsx
@@ -7,18 +7,18 @@ const someDate = '2019-09-19T13:43:37.122Z'
 describe('LocaleDate', () => {
   it('renders "Invalid Date" sentence', () => {
     const { container } = render(<LocaleDate>not a day</LocaleDate>)
-    expect(container.firstChild && container.firstChild.textContent).toEqual('Invalid Date')
+    expect(container).toHaveTextContent('Invalid Date')
   })
 
   it('renders HexAddr specified start/end values', () => {
     const { container } = render(<LocaleDate>{someDate}</LocaleDate>)
-    expect(container.firstChild).not.toBeNull()
-    expect(container.firstChild && container.firstChild.textContent).not.toEqual('Invalid Date')
+    expect(container).toBeInTheDocument()
+    expect(container).not.toHaveTextContent('Invalid Date')
   })
 
   it('renders null if a falsy children is specified', () => {
     const { container } = render(<LocaleDate />)
-    expect(container.firstChild).toBeNull()
+    expect(container).toHaveTextContent('')
   })
 
   it(`should throw if children is a ReactNode`, () => {
--- src/utils/test-utils.js
+++ src/utils/test-utils.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
 import { ThemeProvider } from 'styled-components'
 import { BrowserRouter as Router } from 'react-router-dom'
