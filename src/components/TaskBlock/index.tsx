e42765dbc6afcbf59cb6539d8716bc559f20e1c5
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2019-10-10 14:31:43 -0300

    (add) tasks blocks

--- src/components/TaskBlock/index.tsx
+++ src/components/TaskBlock/index.tsx
@@ -2,27 +2,41 @@ import React, { HTMLAttributes } from 'react'
 import styled, { withTheme } from 'styled-components'
 import Card from '../Common/Card'
 import theme from 'theme'
+import { rgba, darken } from 'polished'
+import ArrowIcon from './img/right.svg'
+import TimeIcon from './img/time.svg'
 
-const TaskBlockStyled = styled(Card)`
+const TaskItem = styled.div`
   display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+`
+
+const TaskCard = styled(Card)`
+  > div {
+    flex-direction: row;
+  }
 `
 
 const StatusBlock = styled.div<StatusProps>`
   align-items: center;
-  border-bottom-left-radius: ${props => props.theme.card.borderRadius};
+  background-color: ${props => rgba(props.color, 0.25)};
+  border-bottom-left-radius: ${props => props.theme.cards.borderRadius};
   border-bottom-width: 1px;
   border-color: ${props => props.color};
   border-left-width: 6px;
   border-right-width: 1px;
-  border-top-left-radius: ${props => props.theme.card.borderRadius};
+  border-style: solid;
+  border-top-left-radius: ${props => props.theme.cards.borderRadius};
   border-top-width: 1px;
+  color: ${props => darken(0.1, props.color)};
   display: flex;
   flex-direction: column;
   flex-grow: 1;
   flex-shrink: 0;
   justify-content: center;
   max-width: 122px;
-  color: ${props => props.color};
+  padding: 10px;
 `
 
 const Number = styled.h2`
@@ -42,6 +56,78 @@ const StatusLabel = styled.p`
   text-transform: uppercase;
 `
 
+const TaskInfo = styled.div`
+  display: grid;
+  flex-grow: 1;
+  grid-template-columns: 1fr 20px 1fr;
+  padding: 8px 20px 8px 15px;
+  row-gap: 13px;
+`
+
+const InfoItem = styled.div`
+  &:first-child,
+  &:nth-child(4n) {
+    padding-right: 20px;
+  }
+
+  &:nth-child(3n) {
+    padding-left: 20px;
+  }
+`
+
+const InfoItemSeparator = styled.div`
+  width: 20px;
+`
+
+const ArrowContainer = styled(InfoItemSeparator)`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+`
+
+const InfoLabel = styled.h3`
+  color: ${props => props.theme.colors.textLight};
+  font-size: 11px;
+  font-weight: 600;
+  line-height: 1.36;
+  margin: 0;
+`
+
+const InfoValue = styled.span`
+  color: ${props => props.theme.colors.textCommon};
+  cursor: pointer;
+  font-size: 12px;
+  font-weight: normal;
+  line-height: 1.42;
+  margin: 0;
+  overflow: hidden;
+  text-decoration: underline;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
+
+const InfoValueClean = styled(InfoValue)`
+  cursor: default;
+  text-decoration: none;
+`
+
+const TaskTime = styled.div`
+  align-items: center;
+  color: ${props => props.theme.colors.textLight};
+  display: flex;
+  font-size: 10px;
+  font-weight: 600;
+  line-height: 1.4;
+  margin-left: auto;
+  margin-right: 4px;
+  margin-top: 1px;
+
+  > img {
+    margin-right: 3px;
+    margin-top: 2px;
+  }
+`
+
 export enum TaskStatus {
   success,
   submitted,
@@ -52,34 +138,30 @@ interface StatusProps {
   color: string
 }
 
-export interface TaskBlockProps extends HTMLAttributes<HTMLDivElement> {
+export interface TaskItemProps extends HTMLAttributes<HTMLDivElement> {
   number: string
   secretContract: string
   status: TaskStatus
   submittedBy: string
   taskID: string
-  theme?: any
   time: string
   txHash: string
 }
 
-export const statusLabels = {
+interface TaskBlockProps extends HTMLAttributes<HTMLDivElement> {
+  item: TaskItemProps
+  theme?: any
+}
+
+const statusLabels = {
   [TaskStatus.failed]: 'Failed',
   [TaskStatus.submitted]: 'Submitted',
   [TaskStatus.success]: 'Success',
 }
 
 const TaskBlock: React.FC<TaskBlockProps> = (props: TaskBlockProps) => {
-  const {
-    status = TaskStatus.submitted,
-    number,
-    secretContract,
-    submittedBy,
-    taskID,
-    time,
-    txHash,
-    ...restProps
-  } = props
+  const { item, ...restProps } = props
+  const { status = TaskStatus.submitted, number, secretContract, submittedBy, taskID, time, txHash } = item
   const statusColors = {
     [TaskStatus.failed]: theme.taskStatus.failed,
     [TaskStatus.submitted]: theme.taskStatus.submitted,
@@ -87,12 +169,37 @@ const TaskBlock: React.FC<TaskBlockProps> = (props: TaskBlockProps) => {
   }
 
   return (
-    <TaskBlockStyled {...restProps}>
-      <StatusBlock color={statusColors[status]}>
-        <Number>{number}</Number>
-        <StatusLabel>{statusLabels[status]}</StatusLabel>
-      </StatusBlock>
-    </TaskBlockStyled>
+    <TaskItem {...restProps}>
+      <TaskCard noPadding={true}>
+        <StatusBlock color={statusColors[status]}>
+          <Number>#{number}</Number>
+          <StatusLabel>{statusLabels[status]}</StatusLabel>
+        </StatusBlock>
+        <TaskInfo>
+          <InfoItem>
+            <InfoLabel>Submitted By</InfoLabel>
+            <InfoValue>{submittedBy}</InfoValue>
+          </InfoItem>
+          <ArrowContainer>
+            <img src={ArrowIcon} alt="" />
+          </ArrowContainer>
+          <InfoItem>
+            <InfoLabel>Secret Contract</InfoLabel>
+            <InfoValue>{secretContract}</InfoValue>
+          </InfoItem>
+          <InfoItem>
+            <InfoLabel>Tx Hash</InfoLabel>
+            <InfoValue>{txHash}</InfoValue>
+          </InfoItem>
+          <InfoItemSeparator />
+          <InfoItem>
+            <InfoLabel>Task ID</InfoLabel>
+            <InfoValueClean>{taskID}</InfoValueClean>
+          </InfoItem>
+        </TaskInfo>
+      </TaskCard>
+      <TaskTime><img src={TimeIcon} alt="" /> {time}</TaskTime>
+    </TaskItem>
   )
 }
 
--- src/components/TasksHome/index.tsx
+++ src/components/TasksHome/index.tsx
@@ -1,12 +1,65 @@
 import React from 'react'
 import styled from 'styled-components'
 import SectionTitle from '../Common/SectionTitle'
+import TaskBlock, { TaskItemProps, TaskStatus } from '../TaskBlock'
+
+const tasks: Array<TaskItemProps> = [
+  {
+    number: '900565',
+    secretContract: '0x85da6ff194ad23304f3b7fe2c7c1a9e728154deadbf92d620c242b0c55a4f961',
+    status: TaskStatus.failed,
+    submittedBy: '0xc88591acfebdca2aa0504c0bbfc0d6361feebb42d738a1eaa1917c29c134110b',
+    taskID: '0x8c8dc67424e2de539395a5338f68965eb294d4739ab8bda7a8db997523bf8d',
+    time: '2d 2h 6m ago',
+    txHash: '0x8965eb294d4739ab8bda7a8db9975c8f3bf8d8c8dc67424e2de539395a5338f6'
+  },
+  {
+    number: '900564',
+    secretContract: '0x85da6ff194ad23304f3b7fe2c7c1a9e728154deadbf92d620c242b0c55a4f961',
+    status: TaskStatus.submitted,
+    submittedBy: '0xc88591acfebdca2aa0504c0bbfc0d6361feebb42d738a1eaa1917c29c134110b',
+    taskID: '0x8c8dc67424e2de539395a5338f68965eb294d4739ab8bda7a8db997523bf8d',
+    time: '2d 4h 10m ago',
+    txHash: '0x8965eb294d4739ab8bda7a8db9975c8f3bf8d8c8dc67424e2de539395a5338f6'
+  },
+  {
+    number: '900563',
+    secretContract: '0x85da6ff194ad23304f3b7fe2c7c1a9e728154deadbf92d620c242b0c55a4f961',
+    status: TaskStatus.success,
+    submittedBy: '0xc88591acfebdca2aa0504c0bbfc0d6361feebb42d738a1eaa1917c29c134110b',
+    taskID: '0x8c8dc67424e2de539395a5338f68965eb294d4739ab8bda7a8db997523bf8d',
+    time: '2d 4h 12m ago',
+    txHash: '0x8965eb294d4739ab8bda7a8db9975c8f3bf8d8c8dc67424e2de539395a5338f6'
+  },
+  {
+    number: '900562',
+    secretContract: '0x85da6ff194ad23304f3b7fe2c7c1a9e728154deadbf92d620c242b0c55a4f961',
+    status: TaskStatus.success,
+    submittedBy: '0xc88591acfebdca2aa0504c0bbfc0d6361feebb42d738a1eaa1917c29c134110b',
+    taskID: '0x8c8dc67424e2de539395a5338f68965eb294d4739ab8bda7a8db997523bf8d',
+    time: '2d 6h 6m ago',
+    txHash: '0x8965eb294d4739ab8bda7a8db9975c8f3bf8d8c8dc67424e2de539395a5338f6'
+  },
+  {
+    number: '900561',
+    secretContract: '0x85da6ff194ad23304f3b7fe2c7c1a9e728154deadbf92d620c242b0c55a4f961',
+    status: TaskStatus.success,
+    submittedBy: '0xc88591acfebdca2aa0504c0bbfc0d6361feebb42d738a1eaa1917c29c134110b',
+    taskID: '0x8c8dc67424e2de539395a5338f68965eb294d4739ab8bda7a8db997523bf8d',
+    time: '2d 6h 16m ago',
+    txHash: '0x8965eb294d4739ab8bda7a8db9975c8f3bf8d8c8dc67424e2de539395a5338f6'
+  }
+]
 
 const TasksHome = () => {
   return (
     <>
       <SectionTitle>Tasks</SectionTitle>
-      The Tasks
+      {
+        tasks.map((item, index) => {
+          return <TaskBlock item={item} key={index} />
+        })
+      }
     </>
   )
 }
